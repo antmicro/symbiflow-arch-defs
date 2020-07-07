2e67764062d8cb93a361d27dbf26b14c3b717914
Lalit Sharma <lsharma@quicklogic.com>
2020-07-07 15:33:21 +0530

    enabling make env

---
+++ quicklogic/ap3/boards.cmake
@@ -0,0 +1,9 @@
+add_quicklogic_board(
+  BOARD ql745a
+  FAMILY ap3
+  DEVICE ql-ap3-32x32
+  PACKAGE ql745a
+  FABRIC_PACKAGE ql745a
+)
+
+
--- quicklogic/common/cmake/install.cmake
+++ quicklogic/common/cmake/install.cmake
@@ -29,7 +29,7 @@ function(DEFINE_QL_TOOLCHAIN_TARGET)
   get_file_target(FASM_TO_BIT_TARGET ${FASM_TO_BIT})
   # Add fasm2bit to all deps, so it is installed with make install
   add_custom_target(
-    "DEVICE_INSTALL_${FASM_TO_BIT_TARGET}"
+    "DEVICE_INSTALL_${FAMILY}_${FASM_TO_BIT_TARGET}"
     ALL
     DEPENDS ${FASM_TO_BIT}
     )
@@ -45,11 +45,11 @@ function(DEFINE_QL_TOOLCHAIN_TARGET)
   set(TOOLCHAIN_WRAPPERS)
 
   foreach(WRAPPER ${WRAPPERS})
-    set(WRAPPER_PATH "${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/${FAMILY}/toolchain_wrappers/${WRAPPER}")
+    set(WRAPPER_PATH "${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/common/toolchain_wrappers/${WRAPPER}")
     list(APPEND TOOLCHAIN_WRAPPERS ${WRAPPER_PATH})
   endforeach()
 
-  set(VPR_COMMON_TEMPLATE "${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/${FAMILY}/toolchain_wrappers/vpr_common")
+  set(VPR_COMMON_TEMPLATE "${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/common/toolchain_wrappers/vpr_common")
   set(VPR_COMMON "${CMAKE_CURRENT_BINARY_DIR}/vpr_common")
   configure_file(${VPR_COMMON_TEMPLATE} "${VPR_COMMON}" @ONLY)
 
