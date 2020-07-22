e51e0cb9420f750373ed55206df308777e8198d9
Lalit Sharma <lsharma@quicklogic.com>
2020-07-22 07:38:17 -0700

    Updating gmux files and adding one missing file

---
+++ quicklogic/ap3/primitives/clock/clock_cell.sim.v
@@ -0,0 +1,19 @@
+`timescale 1ns/10ps
+(* whitebox *)
+(* FASM_FEATURES="INTERFACE.ASSP.INV.ASSPInvPortAlias" *)
+module CLOCK_CELL(I_PAD, O_CLK);
+
+    (* iopad_external_pin *)
+    input  wire I_PAD;
+
+    (* CLOCK=0 *)
+    (* DELAY_CONST_I_PAD="{iopath_IP_IC}" *)
+    output wire O_CLK;
+	
+	specify
+        (I_PAD=>O_CLK)="";
+    endspecify
+
+    assign O_CLK = I_PAD;
+
+endmodule
