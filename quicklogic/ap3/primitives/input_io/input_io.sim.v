02f375f71eb53f005de39789c870bb95beb7c576
Lalit Sharma <lsharma@quicklogic.com>
2020-09-24 02:13:44 -0700

    Commenting in_reg and out_reg for now

--- quicklogic/ap3/primitives/input_io/input_io.sim.v
+++ quicklogic/ap3/primitives/input_io/input_io.sim.v
@@ -1,9 +1,10 @@
 `include "../in_buff/in_buff.sim.v"
-`include "../in_reg/in_reg.sim.v"
+//`include "../in_reg/in_reg.sim.v"
 
 `timescale 1ns/10ps
 (* FASM_PARAMS="" *)
-(* MODES="in_buff;in_reg" *)
+(* MODES="in_buff" *)
+//(* MODES="in_buff;in_reg" *)
 (* whitebox *)
 module INPUT_IO (
 	A2F,
