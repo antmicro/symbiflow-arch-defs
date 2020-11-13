8284476ee7ad158bc157ee6ae7eea0671b0e5f9c
Robert Winkler <rwinkler@antmicro.com>
2020-11-13 14:35:06 +0100

    Add quicklogic primitives tests to CI
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- .github/kokoro/ql.sh
+++ .github/kokoro/ql.sh
@@ -6,8 +6,21 @@ SCRIPT_DIR="$(dirname "${SCRIPT_SRC}")"
 source ${SCRIPT_DIR}/common.sh
 
 echo
-echo "========================================"
-echo "Running quicklogic CI script"
-echo "----------------------------------------"
+echo "========================================================="
+echo "Running ql primitives generation (make all_ql_primitives)"
+echo "---------------------------------------------------------"
+(
+	source env/conda/bin/activate symbiflow_arch_def_base
+	ninja -j${MAX_CORES} all_ql_primitives
+	ninja print_qor > ql_all_primitives_qor.csv
+)
 
-echo "Nothing yet!"
+echo
+echo "========================================================="
+echo "Running ql sim.v generation (make QL_CELLS_SIM_DEPS)"
+echo "---------------------------------------------------------"
+(
+	source env/conda/bin/activate symbiflow_arch_def_base
+	ninja -j${MAX_CORES} QL_CELLS_SIM_DEPS
+	ninja print_qor > ql_all_simv_qor.csv
+)
